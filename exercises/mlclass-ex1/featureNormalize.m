c009d68b10f4107d57de96b06ad13e294bedda65
Dhananjay Nene <dhananjay.nene@gmail.com>
2011-10-22 03:28:29 +0530

    Replaced computation in a loop with a matrix operation.

--- exercises/mlclass-ex1/featureNormalize.m
+++ exercises/mlclass-ex1/featureNormalize.m
@@ -27,9 +27,8 @@ sigma = zeros(1, size(X, 2));
 %       
 mu=mean(X);
 sigma=std(X);
-for i=1:size(X,2)
-  X_norm(:,i)=(X(:,i).-mu(i))./sigma(i);
-end
+x_size = length(X);
+X_norm = (X_norm - repmat(mu,x_size,1)) ./ repmat(sigma,x_size,1);
 % ============================================================
 
 end
